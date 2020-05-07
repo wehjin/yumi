e19212f7587dd8789821fafbbd88ccac81546e65
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2020-05-07 06:38:06 -0700

    Moves Diary into private sub-module.

--- src/diary/mod.rs
+++ src/diary/mod.rs
@@ -1,12 +1,10 @@
-use std::fs::OpenOptions;
-use std::io;
-use std::path::{Path, PathBuf};
-
+pub use self::diary::*;
 pub use self::reader::*;
 pub use self::writer::*;
 
 mod writer;
 mod reader;
+mod diary;
 
 #[cfg(test)]
 mod tests {
@@ -44,43 +42,3 @@ pub struct SayPos {
 	pub end: usize,
 }
 
-pub struct Diary {
-	file_path: PathBuf,
-	file_size: usize,
-}
-
-impl Diary {
-	pub fn reader(&self) -> io::Result<Reader> {
-		let file = OpenOptions::new().read(true).open(&self.file_path)?;
-		let file_size = self.file_size;
-		Ok(Reader { file, file_size })
-	}
-
-	pub fn commit(&mut self, writer: &Writer) {
-		self.file_size = writer.file_size;
-	}
-
-	pub fn writer(&self) -> io::Result<Writer> {
-		let file = OpenOptions::new().append(true).create(true).open(&self.file_path)?;
-		let file_size = self.file_size;
-		file.set_len(file_size as u64)?;
-		Ok(Writer { file, file_size })
-	}
-
-	pub fn len_in_bytes(&self) -> usize { self.file_size }
-
-	pub fn temp() -> io::Result<Diary> {
-		let mut path = std::env::temp_dir();
-		path.push(&format!("diary{}", rand::random::<u32>()));
-		Diary::load(&path)
-	}
-
-	pub fn load(file_path: &Path) -> io::Result<Diary> {
-		let file_path = file_path.to_path_buf();
-		let file_size = {
-			let file = OpenOptions::new().write(true).create(true).open(&file_path)?;
-			file.metadata()?.len() as usize
-		};
-		Ok(Diary { file_path, file_size })
-	}
-}
